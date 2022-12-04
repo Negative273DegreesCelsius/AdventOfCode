08ff60e6fa616a892cfa107797ff7bc660282983
Negative273DegreesCelsius <87785848+Negative273DegreesCelsius@users.noreply.github.com>
2022-12-04 12:37:18 -0500

    2022 Day 4
    2022 Day 4

---
+++ 2022/Day4/Main.java
@@ -0,0 +1,37 @@
+package Day4;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws FileNotFoundException {
+        File file = new File("Day4\\Input.txt");
+        BufferedReader br = new BufferedReader(new FileReader(file));
+
+        // part 1
+        int countContain = 0;
+        int countOverlap = 0;
+        try {
+            String str;
+            while ((str = br.readLine()) != null) {
+                String[] sections = str.split(",");
+                String[] range1 = sections[0].split("-");
+                String[] range2 = sections[1].split("-");
+                int r1Start = Integer.parseInt(range1[0]);
+                int r1End = Integer.parseInt(range1[1]);
+                int r2Start = Integer.parseInt(range2[0]);
+                int r2End = Integer.parseInt(range2[1]);
+                if (((r1Start <= r2Start) && (r1End >= r2End)) || ((r2Start <= r1Start) && (r2End >= r1End)))
+                    countContain++;
+                if (((r1End >= r2Start) && (r1Start <= r2Start)) || ((r2End >= r1Start) && (r2Start <= r1Start)))
+                    countOverlap++;
+            }
+        } catch (IOException e) { }
+
+        System.out.println("Contains: " + countContain);
+        System.out.println("Overlaps: " + countOverlap);
+    }
+}
